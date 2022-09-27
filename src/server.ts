536f4392c1242dba60bc61a65e161f852c027516
dudsjrsilva <dudsjrsilva@gmail.com>
2022-09-26 23:00:57 -0300

    feat: add doPost function

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
-function doGet(e: GoogleAppsScript.Events.AppsScriptHttpRequestEvent) {
-  Logger.log(JSON.stringify(e.parameter))
-  const { name } = e.parameter;
+function doGet(e: GoogleAppsScript.Events.DoGet) {
+  const { name } = e.parameter; //GET Parameter
   return ContentService.createTextOutput(`Hello ${name}`);
+}
+
+interface IBody{
+  name: string;
+}
+
+function doPost(e: GoogleAppsScript.Events.DoPost) {
+  const body = JSON.parse(e.postData.contents) as IBody //POST Body
+  return ContentService.createTextOutput(`Hello ${body.name}`);
 }
